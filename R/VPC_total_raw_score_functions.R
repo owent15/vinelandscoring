085bbcae3cf0594c7df1bf5c60c75dd8156c9b86
owent15 <115123232+owent15@users.noreply.github.com>
2023-08-17 13:12:56 +1000

    updated raw score coding
    included basal and ceiling

--- R/VPC_total_raw_score_functions.R
+++ R/VPC_total_raw_score_functions.R
@@ -1,29 +1,81 @@
-#example for columns that accounts for NA
 process_column <- function(column) {
-  sequence_of_zeros <- 0
-  sum_value <- 0
   
-  for (i in 1:length(column)) {
-    # If the value is NA, skip this iteration of the loop
-    if (is.na(column[i])) {
-      next
-    }
+  # This function determines the highest end of the basal sequence
+  find_basal <- function(column) {
+    last_basal_end <- NA
+    consec_twos <- 0
     
-    if (column[i] == 0) {
-      sequence_of_zeros <- sequence_of_zeros + 1
-    } else {
-      sequence_of_zeros <- 0
+    for (i in 1:length(column)) {
+      if (is.na(column[i])) {
+        consec_twos <- 0
+        next
+      }
+      
+      if (column[i] == 2) {
+        consec_twos <- consec_twos + 1
+        if (consec_twos == 5) {
+          last_basal_end <- i 
+        }
+      } else {
+        consec_twos <- 0
+      }
     }
-    
-    if (sequence_of_zeros < 5) {
-      sum_value <- sum_value + column[i]
-    } else {
-      break
+    return(last_basal_end)
+  }
+  
+  find_ceiling <- function(column) {
+    consec_zeros <- 0
+    for (i in 1:length(column)) {
+      if (is.na(column[i])) {
+        consec_zeros <- 0
+        next
+      }
+      
+      if (column[i] == 0) {
+        consec_zeros <- consec_zeros + 1
+        if (consec_zeros == 5) {
+          return(i - 4)
+        }
+      } else {
+        consec_zeros <- 0
+      }
+    }
+    return(NA)
+  }
+  
+  
+  # Find starting positions of the basal and ceiling sequences
+  basal_start <- find_basal(column)
+  ceiling_start <- find_ceiling(column)
+  
+  sum_value <- 0
+  reached_ceiling <- FALSE
+  
+  if (is.na(basal_start)) {
+    # No basal was found, sum normally but consider the ceiling
+    for (i in 1:length(column)) {
+      if (!is.na(ceiling_start) && i == ceiling_start) {
+        reached_ceiling <- TRUE
+        break
+      }
+      if (!is.na(column[i])) {
+        sum_value <- sum_value + column[i]
+      }
+    }
+  } else {
+    # Basal was found, count scores below basal as 2 and consider the ceiling
+    for (i in 1:length(column)) {
+      if (!is.na(ceiling_start) && i == ceiling_start) {
+        reached_ceiling <- TRUE
+        break
+      }
+      if (i < basal_start) {
+        sum_value <- sum_value + 2
+      } else if (!is.na(column[i])) {
+        sum_value <- sum_value + column[i]
+      }
     }
   }
   
   return(sum_value)
 }
-
-
- 
